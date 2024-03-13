fd8d0cf46a5a861fd947e89c50861f8e2c3452a4
Hadi_Abdelwhab <105137729+HadiAbdelwhab@users.noreply.github.com>
2024-03-13 15:28:35 +0200

    set up bottom navigation and fragment

--- app/src/main/java/com/example/climecast/MainActivity.kt
+++ app/src/main/java/com/example/climecast/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.climecast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+        navController = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(bottomNavigationView, navController)
+
+        //bottomNavigationView.
+
     }
 }
